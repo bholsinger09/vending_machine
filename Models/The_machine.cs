3d7fed96549fe43c7939b5119edbcec61e3f2bb7
ben <bholsinger@gmail.com>
2019-07-09 14:11:17 -0600

    Building some data and switches for Program.cs

--- Models/The_machine.cs
+++ Models/The_machine.cs
@@ -1,6 +1,18 @@
 namespace vending_machine.Models
 {
 
+  class The_machine
+  {
+
+    public string Location { get; private set; }
+    public string Name { get; private set; }
+
+    public The_machine(string location, string name)
+    {
+      Location = location;
+      Name = name;
+
+    }
 
 
 
@@ -11,5 +23,7 @@ namespace vending_machine.Models
 
 
 
+
+  }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using vending_machine.Models;
 
 namespace vending_machine
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+      Console.Clear();
+      #region Create Data
+      The_machine V420 = new The_machine("Boise, Idaho", "Snacking inc.");
+
+
+
+      #endregion
+
+      bool usingMachine = true;
+      string menu = "available";
+      //building menu for available foods(only has chips)
+      //building menu for available drinks
+
+      while (usingMachine)
+      {
+        switch (menu)
         {
-            Console.WriteLine("Hello World!");
+          case "available":
+            Console.Write("The chip your choosen is available");
+            break;
+
+          case "return"
+            Console.Write("Returning money");
+            break;
+
         }
+        string selection = Console.ReadLine().ToLower();
+
+
+
+      }
+
+
+
+
+
     }
+  }
 }
