23dd9eef9bea65b77dd227474207e19a69cd9a5d
ben <bholsinger@gmail.com>
2019-07-09 14:31:00 -0600

    build parent classes with members and constructor

--- Models/chips.cs
+++ Models/chips.cs
@@ -1,7 +1,22 @@
 namespace vending_machine.Models
 {
 
+  abstract class ChipBag
+  {
 
+    public string Name { get; set; }
+    public int Cost { get; set; }
+
+    public ChipBag(string name, int cost)
+    {
+      Name = name;
+      Cost = cost;
+    }
+
+
+
+
+  }
 
 
 
--- Models/drinks.cs
+++ Models/drinks.cs
@@ -1,6 +1,23 @@
 namespace vending_machine.Models
 {
 
+  abstract class Drink
+  {
+    public string Name { get; set; }
+    public string Cost { get; set; }
+
+    public Drink(string name, string cost)
+
+    {
+      Name = name;
+      Cost = cost;
+
+    }
+
+
+
+
+  }
 
 
 
